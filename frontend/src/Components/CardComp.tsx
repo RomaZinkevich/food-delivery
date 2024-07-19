505f4ff4624b48c0770bcf02c539176953eec30a
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2024-07-19 22:31:42 +0300

    Perfectly structured card components

---
+++ frontend/src/Components/CardComp.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../Styles/CardComp.css";
+import FoodContext, { useFood } from "../Context/context";
+import FoodItem from "../Components/FoodItem";
+const CardComp = () => {
+  const { menuItems } = useFood();
+  return (
+    <div className="foodItem">
+      <h2>Lorem ipsum dolor sit amet</h2>
+      <div className="foodList">
+        {menuItems.map((item, i) => (
+          <FoodItem
+            key={i}
+            id={item.id}
+            name={item.name}
+            description={item.description}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CardComp;
