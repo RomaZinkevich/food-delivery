41bf1dfc37a02f701f8410b589df984e460c5f20
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2024-07-19 23:30:20 +0300

    Adding quantity of the food items

--- frontend/src/Components/FoodItem.tsx
+++ frontend/src/Components/FoodItem.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/FoodItem.css";
+import { CiCirclePlus } from "react-icons/ci";
+import { RxCross2 } from "react-icons/rx";
 
 const FoodItem = ({ id, name, description, price }) => {
+  const [quantity, setQuantity] = useState<number>(0);
   return (
     <div className="foodCard">
+      <div className="foodImage">
+        {quantity === 0 ? (
+          <CiCirclePlus
+            className="adding"
+            onClick={() => setQuantity((prev) => prev + 1)}
+          />
+        ) : (
+          <div className="foodCounter">
+            <RxCross2 onClick={() => setQuantity((prev) => prev - 1)} />
+            <p>{quantity}</p>
+            <CiCirclePlus onClick={() => setQuantity((prev) => prev + 1)} />
+          </div>
+        )}
+      </div>
       <div className="foodItemInfo">
         <div className="foodName">
           <p>{name}</p>
