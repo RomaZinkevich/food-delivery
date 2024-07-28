a6585d3188529ba7995f07ca393d3d8b28a50b8b
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2024-07-28 21:44:14 +0300

    have to fix the name error while signing up

--- frontend/src/Components/Signup.tsx
+++ frontend/src/Components/Signup.tsx
@@ -1,19 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/Signup.css";
+import axios from "axios";
 
 const Signup = () => {
+  const [credentials, setCredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSubmission = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    try {
+      const config = {
+        name: credentials.name,
+        email: credentials.email,
+        password: credentials.password,
+      };
+
+      const { data } = await axios.post(
+        "http://46.101.197.215/api/users/",
+        config
+      );
+
+      if (data?.success) {
+        alert("Account created successfully");
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (e: { target: { name: any; value: any } }) => {
+    setCredentials((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
     <div className="login">
-      <form className="loginContainer">
+      <form onSubmit={handleSubmission} className="loginContainer">
         <div className="title">
           <h2>Sign up</h2>
         </div>
         <div className="inputs">
-          <input type="text" placeholder="Enter your name" required />
-          <input type="email" placeholder="Enter your email ID" required />
-          <input type="password" placeholder="Enter your Password" required />
+          <input
+            name="name"
+            onChange={handleChange}
+            type="text"
+            placeholder="Enter your name"
+            required
+          />
+          <input
+            name="email"
+            onChange={handleChange}
+            type="email"
+            placeholder="Enter your email ID"
+            required
+          />
+          <input
+            name="password"
+            onChange={handleChange}
+            type="password"
+            placeholder="Enter your Password"
+            required
+          />
         </div>
-        <button className="button">signup</button>
+        <button type="submit" className="button">
+          signup
+        </button>
         <br />
         <a>Already have an account? Click here</a>
       </form>
