3e827405c35b1a46924b8c6cd384894928be915b
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2024-07-28 21:05:34 +0300

    Signup page designed

--- frontend/src/Components/Signup.tsx
+++ frontend/src/Components/Signup.tsx
@@ -3,19 +3,21 @@ import "../Styles/Signup.css";
 
 const Signup = () => {
   return (
-    <form className="loginContainer">
-      <div className="title">
-        <h2>Login</h2>
-      </div>
-      <div className="inputs">
-        <input type="text" placeholder="Enter your name" required />
-        <input type="email" placeholder="Enter your email ID" required />
-        <input type="password" placeholder="Enter your Password" required />
-      </div>
-      <button className="button">signup</button>
-      <br />
-      <a>Already have an account? Click here</a>
-    </form>
+    <div className="login">
+      <form className="loginContainer">
+        <div className="title">
+          <h2>Sign up</h2>
+        </div>
+        <div className="inputs">
+          <input type="text" placeholder="Enter your name" required />
+          <input type="email" placeholder="Enter your email ID" required />
+          <input type="password" placeholder="Enter your Password" required />
+        </div>
+        <button className="button">signup</button>
+        <br />
+        <a>Already have an account? Click here</a>
+      </form>
+    </div>
   );
 };
 
